c177db4db26729933e26bbc1492e05e9ca4be8f1
Cschuster <connorschuster@gmail.com>
2018-03-15 14:25:32 -0500

    updated

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Wheredafood
 This project will find food estaglishments of certain types within a give radius of the adress thats inputed
+
 #Getting started
+We began by setting up a main html page to run everything off of.
+
+We then chose several different types of cuisine and used the googlemaps api and a custom src to pull up the correct locations in a given area
+
+
--- js/google.api.js
+++ js/google.api.js
@@ -1,7 +1,7 @@
 
 // google search API
 var map = document.getElementById('mapWindow');
-var loc = "houston";
+var loc = prompt("Which city are you in?");
 var type = "";
 var modaltitle = document.getElementById('modalTitle')
 
@@ -23,98 +23,98 @@ var caribbean = document.getElementById('caribbean')
 
 
 american.addEventListener('click', function() {
-    modaltitle.textContent = "American";
+    modaltitle.textContent = "American Cuisine";
     type = "american+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 mexican.addEventListener('click', function() {
-    modaltitle.textContent = "Mexican";
+    modaltitle.textContent = "Mexican Cuisine";
     type = "mexican+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 indian.addEventListener('click', function() {
-    modaltitle.textContent = "Indian";
+    modaltitle.textContent = "Indian Cuisine";
     type = "indian+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 chinese.addEventListener('click', function() {
-    modaltitle.textContent = "Chinese";
+    modaltitle.textContent = "Chinese Cuisine";
     type = "chinese+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
    
 })
 
 german.addEventListener('click', function() {
-    modaltitle.textContent = "German";
+    modaltitle.textContent = "German Cuisine ";
     type = "german+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 italian.addEventListener('click', function() {
-    modaltitle.textContent = "Italian";
+    modaltitle.textContent = "Italian Cuisine";
     type = "italian+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 african.addEventListener('click', function() {
-    modaltitle.textContent = "African";
+    modaltitle.textContent = "African Cuisine";
     type = "african+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 vegan.addEventListener('click', function() {
-    modaltitle.textContent = "Vegan";
+    modaltitle.textContent = "Vegan Cuisine";
     type = "vegan+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 thai.addEventListener('click', function() {
-    modaltitle.textContent = "Thai";
+    modaltitle.textContent = "Thai Cuisine";
     type = "thai+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 mongolian.addEventListener('click', function() {
-    modaltitle.textContent = "Mongolian";
+    modaltitle.textContent = "Mongolian Cuisine";
     type = "mongolian+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 french.addEventListener('click', function() {
-    modaltitle.textContent = "French";
+    modaltitle.textContent = "French Cuisine";
     type = "french+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 caribbean.addEventListener('click', function() {
-    modaltitle.textContent = "Caribbean";
+    modaltitle.textContent = "Caribbean Cuisine";
     type = "caribbean+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
 })
 
 greek.addEventListener('click', function() {
-    modaltitle.textContent = "Greek";
+    modaltitle.textContent = "Greek Cuisine";
     type = "greek+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     console.log(greek)
 })
 
 cuban.addEventListener('click', function() {
-    modaltitle.textContent = "Cuban";
+    modaltitle.textContent = "Cuban Cuisine";
     type = "cuban+cuisine";
     map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
     
