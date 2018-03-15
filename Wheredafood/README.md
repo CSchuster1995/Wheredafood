ad0481e31f7699ce9851c5862f2faaff13881617
CSchuster1995 <36170365+CSchuster1995@users.noreply.github.com>
2018-03-15 10:09:51 -0500

    Add files via upload

---
+++ Wheredafood/README.md
@@ -0,0 +1,3 @@
+# Wheredafood
+This project will find food estaglishments of certain types within a give radius of the adress thats inputed
+#Getting started
---
+++ Wheredafood/js/google.api.js
@@ -0,0 +1,90 @@
+
+// google search API
+var map = document.getElementById('mapWindow');
+var loc = "houston";
+var type = "";
+
+var american = document.getElementById('american')
+var mexican = document.getElementById('mexican')
+var indian = document.getElementById('indian')
+var chinese = document.getElementById('chinese')
+var german = document.getElementById('german')
+var italian = document.getElementById('italian')
+
+
+
+american.addEventListener('click', function() {
+  
+    type = "american+cuisine";
+    map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
+    
+})
+
+mexican.addEventListener('click', function() {
+    type = "mexican+cuisine";
+    map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
+
+})
+
+indian.addEventListener('click', function() {
+    type = "indian+cuisine";
+    map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
+
+})
+
+chinese.addEventListener('click', function() {
+    type = "chinese+cuisine";
+    map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
+
+})
+
+german.addEventListener('click', function() {
+    type = "german+cuisine";
+    map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
+
+})
+
+italian.addEventListener('click', function() {
+    type = "italian+cuisine";
+    map.src = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBhAxU0T3tdTd04TZMIi0ZXXu3fuDpELok&q=" + "+" + type + "+" + "near" + "+" + loc;
+console.log(italian)
+})
+
+
+
+
+// Zomato API
+
+
+// var zomKey = "bd5dfbb9276d36c0b9e8c4e99625e114";
+// var newData = "";
+
+// $(document).ready(function () {
+//     $.ajax({
+//         url: "https://developers.zomato.com/api/v2.1/cities?q=" + loc,
+//         type: "GET",
+//         dataType: "json",
+//         headers: { "user-key": "bd5dfbb9276d36c0b9e8c4e99625e114" },
+//         success:function(data){
+            
+//             newData = data.location_suggestions[0].id;
+
+//             $(document).ready(function () {
+//                 $.ajax({
+//                     url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + newData + "&entity_type=city&establishment_type=81",
+//                     type: "GET",
+//                     dataType: "json",
+//                     headers: { "user-key": "bd5dfbb9276d36c0b9e8c4e99625e114" },
+//                     success:function(data){
+//                         newList = data;
+                        
+//                     },
+                    
+//                 }); 
+//             });
+            
+//         },
+        
+//     }); 
+// });
+// var newList = "";
